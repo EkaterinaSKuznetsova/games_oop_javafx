6fb51666a54fe4c98b3cbfd385e82814be9839f4
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-10-26 20:57:06 +0300

    2. Каркас шахматной доски [#792]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -9,6 +9,7 @@ public class Win {
         }
         return false;
     }
+
     public static boolean monoHorizontal(int[][] board, int row) {
          for (int i = 0; i < board.length; i++) {
             if (board[row][i] != 1) {
@@ -19,7 +20,7 @@ public class Win {
     }
 
     public static boolean monoVertical(int[][] board, int column) {
-        for(int i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][column] != 1) {
                 return false;
             }
