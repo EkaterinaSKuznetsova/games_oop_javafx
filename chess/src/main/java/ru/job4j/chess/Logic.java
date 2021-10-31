eec62d1436b288a2439a7105212d08dbdb4bb1a9
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-10-31 14:48:57 +0300

    комментарии

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -1,7 +1,5 @@
 package ru.job4j.chess;
 
-package ru.job4j.chess;
-
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 import java.util.Arrays;
--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,10 +1,7 @@
 package ru.job4j.chess.firuges.black;
-
 import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
-import java.lang.invoke.MethodType;
 
 public class BishopBlack implements Figure {
     private final Cell position;
@@ -20,31 +17,12 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-       if (!isDiagonal(this.position, dest)) {
-           throw new ImpossibleMoveException(
-                   String.format("Could not way by diagonal from %s to %s", position, dest)
-           );
-       }
-       int size = Math.abs(dest.getX() - this.position.getX());
-       Cell[] steps = new Cell[size];
-       int deltaX = dest.getX() >= this.position.getX() ? 1 : -1;
-       int deltaY = dest.getY() >= this.position.getY() ? 1 : -1;
-       int x = this.position.getX();
-       int y = this.position.getY();
-       for (int i = 0; i < size; i++) {
-            x = x + deltaX;
-            y = y + deltaY;
-           steps[i] = Cell.findBy(x, y);
-       }
-      return steps;
+        throw new ImpossibleMoveException(
+                String.format("Could not way by diagonal from %s to %s", position, dest)
+        );
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        int deltaX = Math.abs(source.getX() - dest.getX());
-        int deltaY = Math.abs(source.getY() - dest.getY());
-        if (deltaX == deltaY) {
-            return true;
-        }
         return false;
     }
 
@@ -53,3 +31,4 @@ public class BishopBlack implements Figure {
         return new BishopBlack(dest);
     }
 }
+
